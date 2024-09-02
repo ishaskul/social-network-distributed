c70988f60894d86f443dc1039d9ed7b77bcd069d
Ishas <ishaskul@gmail.com>
2024-09-02 13:39:05 +0200

    Updated comments

--- generate-prometeus-query-for-container-cpu-util.py
+++ generate-prometeus-query-for-container-cpu-util.py
@@ -9,7 +9,7 @@ def get_list_of_containers_and_pids():
     dockerClient = docker.from_env()
     hostname = get_hostname()
     
-    # Open a CSV file to write the data
+    # Create a csv file with the hostname appended at the trailing end
     with open(f'./stress_test_data/compose_post_scenario/system_cpu_data/container_pids_{hostname}.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['hostname', 'container_name', 'pid'])
@@ -24,10 +24,9 @@ def get_list_of_containers_and_pids():
                 writer.writerow([hostname, name, pid])
                 pids_list = pids_list + f"{pid}|"
                 
-        # Remove the trailing "|" character from pids_list
+        # Remove the last "|" from the list
         pids_list = pids_list.rstrip('|')
         
-        # Generate both Prometheus queries
         cpu_usage_query = generate_prometheus_query(pids_list, "scaph_process_cpu_usage_percentage")
         power_consumption_query = generate_prometheus_query(pids_list, "scaph_process_power_consumption_microwatts") + " / 1000000"
 
